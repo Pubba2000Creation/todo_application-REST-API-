bc01fd1215d7b8e85607434455f9f00e986787c8
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2024-03-29 05:26:28 +0530

    Update app.controller.spec.ts
    try to run test one

--- todo_api/src/app.controller.spec.ts
+++ todo_api/src/app.controller.spec.ts
@@ -4,6 +4,7 @@ import { TodoService } from './todo.service';
 
 describe('TodoController', () => {
   let todoController: TodoController;
+  let todoService: TodoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,7 +13,33 @@ describe('TodoController', () => {
     }).compile();
 
     todoController = module.get<TodoController>(TodoController);
+    todoService = module.get<TodoService>(TodoService);
+    
   });
 
   // Write test case
+
+  it('should return a todo by ID', async () => {
+    const todoId = '6605f90814a882242aa0bc57';
+    const todo: Todo = { id: todoId, title: 'new task one', completed: false };
+    jest.spyOn(todoService, 'findById').mockResolvedValue(todo);
+  
+    const result = await todoController.findById(todoId);
+  
+    expect(result).toEqual(todo);
+  });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
